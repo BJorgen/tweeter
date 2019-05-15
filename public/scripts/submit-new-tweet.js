9ae9b8a8531fb93289a18a8dbe5f7436c1b9097b
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-15 16:52:30 -0600

    Update new tweet POST request to use success field

--- public/scripts/submit-new-tweet.js
+++ public/scripts/submit-new-tweet.js
@@ -2,14 +2,15 @@ $(function() {
     const $form = $('.new-tweet form');
     $form.on('submit', function () {
         event.preventDefault();
-        const text = $(this).children('textarea').val()
-        console.log(this)
-        console.log(text)
-    //   $.ajax('more-posts.html', { method: 'GET' })
-    //   .then(function (morePostsHtml) {
-    //     console.log('Success: ', morePostsHtml);
-    //     $button.replaceWith(morePostsHtml);
-    //   });
+        const formContent = $(this).serialize();
+        const $textArea = $(this).children('textarea');
+        
+        $.ajax({
+            data: formContent,
+            url: '/tweets',
+            method: 'POST',
+            success: () => $textArea.val("")
+        });
 
     });
   });
