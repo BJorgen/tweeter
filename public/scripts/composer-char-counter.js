e5466b18d10e335cb55a7b9496fd707c01f826a4
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-14 19:25:15 -0600

    Add character count and formatting for new-tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
-    console.log("From the callback")
-    // --- our code goes here ---
+    $('.new-tweet textarea').on('input', function() {
+        const spaceLeft = 140 - $(this).val().length;
+        const counter = $(this).siblings('.counter')
+        counter.html(spaceLeft);
+        if (spaceLeft < 0) {
+            counter.addClass('invalid');
+        } else {
+            counter.removeClass('invalid');
+        }
+      })
   });
