f01b7f4541ab73e2fb4157f0a76a9d5e9474f141
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-15 21:32:48 -0600

    Add form validation to new tweet submit

--- public/scripts/submit-new-tweet.js
+++ public/scripts/submit-new-tweet.js
@@ -4,13 +4,21 @@ $(function() {
         event.preventDefault();
         const formContent = $(this).serialize();
         const $textArea = $(this).children('textarea');
-        
-        $.ajax({
-            data: formContent,
-            url: '/tweets',
-            method: 'POST',
-            success: () => $textArea.val("")
-        });
-
+        const tweet = $textArea.val();
+        console.log(tweet)
+        if (tweet) {
+            if(tweet.length > 140) {
+                alert('Edit yourself! Too much Tweet!')
+            } else {
+                $.ajax({
+                    data: formContent,
+                    url: '/tweets',
+                    method: 'POST',
+                    success: () => $textArea.val("")
+                });
+            }
+        } else {
+            alert("You can't tweet NOTHING!!")
+        }
     });
   });
