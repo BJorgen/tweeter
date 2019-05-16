0824dfdb61ff0adc2a7880a455869a0925ef9283
Britta Jorgenson <britta.jorgenson@gmail.com>
2019-05-15 20:52:13 -0600

    Add loadTweets function to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,5 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-    renderTweets(data);
+    
+    function loadTweets(){
+        $.ajax({
+            method : 'GET',
+            url : '/tweets',
+            contentType : 'application/json',
+            success: (data) => renderTweets(data)
+        });
+    }
+
+    loadTweets();
+    //renderTweets(data);
 });
